094409532485b29f21c47dc9f4deb97cf12ca9b6
H.Hassan <62448602+HamzaHassanM@users.noreply.github.com>
2021-12-15 21:07:52 +0200

    first Step : creating project & creating header,footer,index,aboutus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import{ AboutComponent } from './pages/about/about.component'
+import{ IndexComponent } from './pages/index/index.component'
+const routes: Routes = [
+  {path:"",component:IndexComponent},
+  {path:"about",component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layouts/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { IndexComponent } from './pages/index/index.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
